1df7838d8320606975089f2519df2798a7d8894b
Lorenzo Vanlare Sprenger <lorenzosprenger@gmail.com>
2024-04-23 11:30:21 -0300

    23-04

--- login/script.js
+++
@@ -1,8 +0,0 @@
-const togglePassword = document.getElementById('togglePassword');
-const password = document.getElementById('password');
-
-togglePassword.addEventListener('click', function() {
-    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
-    password.setAttribute('type', type);
-    this.classList.toggle('fa-eye-slash');
-});
