024f98bff8b08f88587e45065964cb75b6420987
unknown <apossebonn@gmail.com>
2024-05-10 21:33:35 -0300

    projetoPP_10.05

--- back/src/config/db.js
+++ back/src/config/db.js
@@ -9,10 +9,10 @@ const dotenv = require('dotenv').config();
 
 // Estabelece a criação da conexão com banco 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    // password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
 });
 
 // Testa se o banco esta conectado
