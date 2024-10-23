ed689658516a8acd0b50035a53593dd4d8cab49e
Lorenzo Vanlare Sprenger <lorenzosprenger@gmail.com>
2024-10-23 11:39:38 -0300

    rotasSwagger

--- back/src/routes/usersRouter.js
+++ back/src/routes/usersRouter.js
@@ -1,8 +1,10 @@
 /**
- INFORMAÇÕES DO ROUTER
-
- Uma rota em uma API é um “caminho” que será “chamado” por uma aplicação ou cliente e responderá alguma informação. Cada rota pode ter uma ou mais funções, e ela deve ser única na API com o seu método HTTP definido, ao receber uma chamada ela faz todo o processamento necessário para retornar os dados que foi solicitado
-*/
+ * INFORMAÇÕES DO ROUTER
+ * 
+ * Uma rota em uma API é um “caminho” que será “chamado” por uma aplicação ou cliente e responderá alguma informação. 
+ * Cada rota pode ter uma ou mais funções, e ela deve ser única na API com o seu método HTTP definido, 
+ * ao receber uma chamada ela faz todo o processamento necessário para retornar os dados que foi solicitado.
+ */
 
 // Importar o modulo de Router do express
 const { Router } = require('express');
@@ -23,15 +25,173 @@ const {
     getPetsByUserId
 } = require('../controller/usersController')
 
-// Criar os endpoints (rotas) que serão acessados a partir dos métodos HTTP (get,post,put,delete)
+/**
+ * @swagger
+ * /users/listUsers:
+ *  get:
+ *    summary: Retorna todos os usuários
+ *    responses:
+ *      200:
+ *        description: Uma lista de usuários
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                type: object
+ */
 router.get('/users/listUsers', listUsers);
+
+/**
+ * @swagger
+ * /users/pet:
+ *  post:
+ *    summary: Cadastra um novo pet
+ *    responses:
+ *      200:
+ *        description: Sucesso!
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ */
 router.post('/users/pet', cadastroPet);
+
+/**
+ * @swagger
+ * /users/usuario:
+ *  post:
+ *    summary: Cadastra um novo usuário
+ *    responses:
+ *      200:
+ *        description: Sucesso!
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ */
 router.post('/users/usuario', cadastroUsuario);
+
+/**
+ * @swagger
+ * /users/calendario:
+ *  post:
+ *    summary: Adiciona um evento ao calendário
+ *    responses:
+ *      200:
+ *        description: Evento cadastrado com sucesso!
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ */
 router.post('/users/calendario', eventoCalendario);
-router.get('/users/calendario/get', carregarEventos);  
+
+/**
+ * @swagger
+ * /users/calendario/get:
+ *  get:
+ *    summary: Carrega todos os eventos do calendário
+ *    responses:
+ *      200:
+ *        description: Lista de eventos
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                type: object
+ */
+router.get('/users/calendario/get', carregarEventos);
+
+/**
+ * @swagger
+ * /users/calendario/{id_evento}:
+ *  delete:
+ *    summary: Remove um evento do calendário pelo ID
+ *    parameters:
+ *      - in: path
+ *        name: id_evento
+ *        required: true
+ *        description: ID do evento a ser removido
+ *        schema:
+ *          type: integer
+ *    responses:
+ *      200:
+ *        description: Evento removido com sucesso!
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ */
 router.delete('/users/calendario/:id_evento', deleteEventoCalendario);
+
+/**
+ * @swagger
+ * /user/{id}:
+ *  put:
+ *    summary: Atualiza um usuário pelo ID
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        required: true
+ *        description: ID do usuário a ser atualizado
+ *        schema:
+ *          type: integer
+ *    responses:
+ *      200:
+ *        description: Usuário atualizado com sucesso!
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ */
 router.put('/user/:id', updateUser);
+
+/**
+ * @swagger
+ * /user/{id}/delete:
+ *  delete:
+ *    summary: Remove um usuário pelo ID
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        required: true
+ *        description: ID do usuário a ser removido
+ *        schema:
+ *          type: integer
+ *    responses:
+ *      200:
+ *        description: Usuário removido com sucesso!
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ */
 router.delete('/user/:id/delete', deleteUser);
+
+/**
+ * @swagger
+ * /users/{id}/pets:
+ *  get:
+ *    summary: Retorna todos os pets de um usuário específico
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        required: true
+ *        description: ID do usuário
+ *        schema:
+ *          type: integer
+ *    responses:
+ *      200:
+ *        description: Lista de pets do usuário
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                type: object
+ */
 router.get('/users/:id/pets', getPetsByUserId);
 
 module.exports = router;
--- back/src/server.js
+++ back/src/server.js
@@ -1,9 +1,33 @@
-// Módulo de inicialização do servidor web onde nossa webapi estará hospedada, módulo de infraestrutura;
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const swaggerUi = require("swagger-ui-express")
+const swaggerJsDoc = require("swagger-jsdoc")
 
-// Importar o arquivo app
-const app = require('./app');
-// Importar a porta do servidor
-const port = app.get('port');
+const swaggerOptions = {
+    swaggerDefinition: {
+        openapi: "3.0.0",
+        info: {
+            title: "API de Tarefas",
+            version: "1.0.0",
+            description: "API CRUD para gerenciar tarefas",
+        },
+        servers: [{ url: "http://localhost:3000"}],
+    },
+    apis: [` ${__dirname}/routes/*.js`], //caminho para as rotas
+};
 
-// Testar API
-app.listen(port, () => console.log(`Run on port ${port}!`));
+const app = express()
+const port = process.env.PORT || 1903
+
+app.use(express.json())
+app.use(cors())
+
+const usersRouter = require('./routes/usersRouter');
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs))
+app.use (express.json())
+app .use(cors())
+app.use('/api', usersRouter)
+
+app.listen(port, () => console.log(`Run on port ${port}!`))
